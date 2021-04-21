a648922d124676bd0222cac10aafbf54403400ea
Andrea <oquendo.3455@gmail.com>
2021-04-21 11:52:58 -0300

    added USER

---
+++ src/services/SettingService.ts
@@ -0,0 +1,38 @@
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository"
+
+interface ISettingsCreate{
+    chat:boolean,
+    username: string
+}
+
+class SettingsService {
+
+    async create({ chat, username}: ISettingsCreate){   
+
+        const settingsRepository = getCustomRepository(SettingsRepository);
+
+        //check if user already exists
+        //SELECT * from settings WHERE username = "username" limit 1
+        const userAlreadyexists = await settingsRepository.findOne({
+            username
+        });
+
+        if(userAlreadyexists){
+            throw new Error("User already exists!");
+        }
+
+        // create rep
+        const settings = settingsRepository.create({
+            chat,
+            username
+        })
+        //save obj
+        await settingsRepository.save(settings);
+        
+        return settings;
+    }
+
+}
+
+export { SettingsService };
