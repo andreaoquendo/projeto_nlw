5e7dbdccb882bf093b631b65d83e1a95677ab358
Andrea <oquendo.3455@gmail.com>
2021-04-20 23:27:51 -0300

    updated routes with SettingsController

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { response, Router } from "express";
 import { getCustomRepository } from "typeorm";
+import { SettingsController } from "./controllers/SettingsController";
 import { SettingsRepository } from "./repositories/SettingsRepository";
 
 const routes = Router();
+
+const settingsController = new SettingsController();
 
 /*
     Parameter types:
@@ -11,20 +14,6 @@ const routes = Router();
     - body params
 */
 
-routes.post("/settings", async (request, response) => {
-    const { chat , username }= request.body;
-
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    // create rep
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-    //save obj
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
-});
+routes.post("/settings", settingsController.create);
 
 export { routes }
