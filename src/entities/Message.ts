65db6939a64f6171130b88ca128534a283c936bc
Andrea <oquendo.3455@gmail.com>
2021-04-24 19:47:00 -0300

    Message entity updated

--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryColumn, CreateDateColumn, Column } from "typeorm";
-import { v4 } from "uuid";
+import { Entity, PrimaryColumn, CreateDateColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { User } from "./User";
 
 
 @Entity("messages")
@@ -14,14 +15,21 @@ class Message{
         @Column()
         text: string;
 
+        
+        @JoinColumn({ name: "user_id" })
+        @ManyToOne(()=> User) //muitas mensages para um usuário
+        user: User;
 
+        @Column() // toda vez que for criado o BD vai verificar se o id existe na tabela usuário, senão vai dar erro
         user_id: string;
 
         @CreateDateColumn()
         created_at: Date;
 
         constructor() {
-
+            if(!this.id) {
+                this.id = uuid();
+            }
         }
 }
 
---
+++ src/repositories/MessagesRepository.ts
@@ -0,0 +1,8 @@
+import { Repository } from "typeorm";
+import { Message } from "../entities/Message";
+
+class MessagesRepository extends Repository<Message>{
+
+}
+
+export { MessagesRepository }
