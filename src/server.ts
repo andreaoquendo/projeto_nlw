3be4f6fcda580bb933960e5aafe6ec789f0f79fe
Andrea <oquendo.3455@gmail.com>
2021-04-25 02:29:51 -0300

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
 // node does not run "import/from" ( typescript -D & tsc --init )
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+    return response.render("html/client.html");
+    
+})
+
+const http = createServer(app); // criando protocolo http
+const io = new Server(http); // criando o protocolo web socket
+
+io.on("connection", (socket: Socket) => {
+    console.log("Connected", socket.id);
+});
 
 app.use(express.json());
 
@@ -39,4 +59,5 @@ app.post("/users", (request, response) => {
 */
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running on port 3333."));
+// app.listen(3333, () => console.log("Server is running on port 3333."));
+http.listen(3333, () => console.log("Server is running on port 3333.")); // io embutido
